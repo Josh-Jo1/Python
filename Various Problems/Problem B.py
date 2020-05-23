be2c5fb3dc091d9acd9b26fcea9a49d3b4aac4a7
Joshua Johnson <N/A>
2020-05-23 17:12:09 -0400

    update Problem B

--- Various Problems/Problem B.py
+++ Various Problems/Problem B.py
@@ -3,7 +3,7 @@
 # Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
 # Sum of sequence: 0, 1, 2, 4, 7, 12, 20, 33, 54, 88, 143
 
-# Input an integer to find the sum till that term. Input a letter to end the program.
+# Input an integer to find sum till that term. Input a letter to end program.
 inputTerm = input()
 
 # Method 1
